a897a79f61d01ef7cf8b53e74233a0050d4af64b
MrMasterplan <simon@SimonHekampsMBP.lan>
2019-09-17 20:45:47 +0200

    nicer messages

--- copy_all.sh
+++ copy_all.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 cd openwrt
+for f in $(find . -name .DS_Store)
+do 
+    rm $f
+done
+
 scp -r * wrt:/
 
--- openwrt/etc/openvpn/select_this.sh
+++ openwrt/etc/openvpn/select_this.sh
@@ -3,14 +3,14 @@
 echo "Content-type: text/plain"
 echo ""
 ovpn=$(basename $0)
-echo "You selected $ovpn"
+echo "You selected $ovpn..."
 
 cd /etc/openvpn/
 
-echo "PWD now:"
-pwd
+#echo "PWD now:"
+#pwd
 
-echo "Remove old link..."
+echo "Removing old link..."
 rm nordvpn.ovpn
 
 echo "relink new..."
@@ -19,5 +19,34 @@ ln -s nordvpn/$ovpn nordvpn.ovpn
 echo "fixing password request..."
 sed 's/auth-user-pass$/auth-user-pass secret/' nordvpn.ovpn -i
 
-echo "restart OpenVpn"
+logread -f -e nordvpn >/root/logs &
+logPID=$!
+
+echo "restarting OpenVpn..."
 /etc/init.d/openvpn restart
+
+for i in $(seq 1 8)
+do
+    #echo "file now has $(wc -l /root/logs) lines"
+    #grep "Initialization Sequence Completed" /root/logs
+    if grep "Initialization Sequence Completed" /root/logs 2>&1 >/dev/null
+        then
+            break
+        fi
+    sleep 1
+done
+
+kill -2 $logPID
+
+if grep "Initialization Sequence Completed" /root/logs 2>&1 >/dev/null
+    then
+        echo "=========================="
+        echo "Setup of $ovpn completed."
+        echo ""
+        echo "You can close this page now."
+    else
+        echo "=========================="
+        echo "Something went wrong."
+        echo ""
+        echo "Feel free to go back and try again."        
+    fi
